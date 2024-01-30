f94ed2d22caec347743e02f5a6b1e72bc73e0d15
Adedini James Oloruntoba <145149678+ADEDINIJAMES@users.noreply.github.com>
2024-01-30 23:15:08 +0100

    Smart Quiz Landing Page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import './App.css'
-import LandingPage from './Pages/LandingPage'
+import "./App.css";
+import HomePage from "./Pages/HomePage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  
-
   return (
     <>
-    <LandingPage/>
-
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
