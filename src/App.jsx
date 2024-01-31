ca93539480c22e45fe166c791b6f1b906d870607
Ikenna <ikenna.amadi@decagon.dev>
2024-01-31 17:49:04 +0100

    signup page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import HomePage from "./Pages/HomePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignUpPage from "./Pages/SignUpPage.jsx";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<HomePage />} />
+            <Route exact path="/signup" element={<SignUpPage/>} />
         </Routes>
       </Router>
     </>
