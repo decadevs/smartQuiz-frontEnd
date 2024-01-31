9da5d50513971bc675a6c1c6b4b492e85ae1bfbb
Desmond Isama <desmond.isama@decagon.dev>
2024-01-31 23:08:52 +0100

    Reset password and Forgot password

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import "./App.css";
 import HomePage from "./Pages/HomePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ForgotPPage from "./Pages/ForgotPPage.jsx";
+import ResetPPage from "./Pages/ResetPPage.jsx";
+import PasswordTokenPage from "./Pages/PasswordTokenPage.jsx";
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<HomePage />} />
+            <Route exact path="/forgot-password" element={<ForgotPPage />} />
+            <Route exact path="/reset-password" element={<PasswordTokenPage />} />
+            <Route exact path="/reset-password/:token" element={<ResetPPage />} />
         </Routes>
       </Router>
     </>
