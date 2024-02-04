fa1be08336b70f46e0a6ecc19341a25e39aae351
Emmanuel Bobade <emmanuel.bobade@decagon.dev>
2024-02-04 22:50:28 +0100

    List of quiz  and Dashboard pages done.

---
+++ src/utils/apiFunctions.js
@@ -0,0 +1,40 @@
+// // apiFunctions.js
+
+// // A placeholder function to simulate fetching subjects from an API
+// export const getAllSubjects = async () => {
+//     try {
+//       // Simulate an API call (you should replace this with your actual API call)
+//       const response = await fetch('http://localhost:8080/subject/fetch-all-subjects');
+      
+//       // Check if the response is successful (status code 200)
+//       if (response.ok) {
+//         // Parse the JSON response and return the data
+//         return await response.json();
+//       } else {
+//         // If the response is not successful, throw an error
+//         throw new Error(`Failed to fetch subjects. Status: ${response.status}`);
+//       }
+//     } catch (error) {
+//       // Handle any errors that occur during the API call
+//       console.error('Error fetching subjects:', error.message);
+//       throw error; // You may choose to handle errors differently based on your application's needs
+//     }
+//   };
+  
+import axios from "axios";
+
+// This is to create the base URL for connecting to the backend
+export const api = axios.create({
+  baseURL: "http://localhost:8080"
+});
+
+export async function getAllSubjects(){
+    try{
+        const result = await api.get("/subject/fetch-all-subjects");
+        // ".data" is neccessary to return the data from the backend
+        return result.data;
+    }catch(error){
+        throw new Error("Error fetching Subject");
+    }
+}
+
